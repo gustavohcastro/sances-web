b322dcbde4e7a6573ba9d2874d77e914fcac7cdf
Gustavo <gustavohecastro@gmail.com>
2020-08-13 15:05:13 -0300

    frontend orders page

--- src/pages/Orders/index.js
+++ src/pages/Orders/index.js
@@ -1,12 +1,102 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Menu from '../../components/Menu';
 
+import {FiCheck, FiXCircle} from 'react-icons/fi'
 
+import './orders.css'
+
+import api from '../../services/api'
+import { Link } from 'react-router-dom';
 function Orders() {
+  
+  const [token, setToken] = useState('')
+
+
+  const [itemsOrder, setItemsOrder] = useState([])
+  const [orders, setOrders] = useState([])
+  const [selectedItem, setSelectedItem] = useState('')
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect( ()=> {
+    async function getAuth(){
+      const storagedToken = await JSON.parse(localStorage.getItem("SANCES_TOKEN"))
+      setToken(storagedToken)
+    }
+    getAuth()
+  }, [])
+  useEffect( () => {
+    async function loadOrders(){
+      const Bearer = 'Bearer '.concat(token)
+      await api.get('items-order' , {headers : { Authorization : Bearer}}).then( ({data}) => {
+        setItemsOrder(data)
+      })
+      await api.get('orders' , {headers : { Authorization : Bearer}}).then( ({data}) => {
+        setOrders(data)
+      })
+      setLoading(false)
+    }
+    loadOrders()
+  }, [token])
+
+
+  function handleEditItem({id}){
+    setSelectedItem(id)
+  }
+  async function handleStatus(status){
+      await api.put(`/orders/${selectedItem}`, {status, token}).then( data => {
+        if(data){
+          setToken(token)
+        };
+      })
+  }
+
   return(
-    <>
+    <div id="products-page">
       <Menu/>
-    </>  
+      <div className="content">
+            <h1>Pedidos</h1>
+            <h3>Para detalhes clique sobre os pedidos</h3>
+            <div className="options-row">
+              <div>
+                <button onClick={() => handleStatus("aproved")}>Aprovar</button>
+                <button onClick={() => handleStatus("canceled")}>Cancelar</button>
+              </div>
+            </div>
+            <div id="products-list">
+                <ul>
+                  <li className="product-item strong">
+                    <p>Código</p>
+                    <p>Status</p>
+                    <p>Items</p>
+                  </li>
+                  { orders.map( item => { 
+                    let translateStatus;
+                    if(item.status === "processing"){
+                      translateStatus = "Em Analise"
+                    }
+                    if(item.status === "aproved"){
+                      translateStatus = "Aprovado"
+                    }
+                    if(item.status === "canceled"){
+                      translateStatus = "Cancelado"
+                    }
+                    return (
+                    <li onClick={() => handleEditItem(item)} key={item.id} className={ selectedItem === item.id ? "product-item selected" : "product-item"}>
+                      <p>{item.id}</p>
+                      <p>{translateStatus}</p>
+                      <p>{
+                        itemsOrder.map( itemOrder => (
+                          itemOrder.order_id === item.id && `${itemOrder.name} `
+                        ))
+                        }</p>
+                    </li>
+                    )
+                    })}
+               </ul>
+            </div>
+        </div>
+    </div>  
   );
 }
 
--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,21 +1,123 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Menu from '../../components/Menu';
+
+import api from '../../services/api'
+
 
 import './products.css'
 
 function Products() {
+
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [stock, setStock] = useState('');
+
+  const [selectedItem, setSelectedItem] = useState([])
+
+  const [products, setProducts] = useState([])
+
+  const [token, setToken] = useState('')
+
+  const [alert, setAlert] = useState("");
+
+
+useEffect( ()=> {
+    async function getAuth(){
+      const storagedToken = await JSON.parse(localStorage.getItem("SANCES_TOKEN"))
+      setToken(storagedToken)
+      
+    }
+    getAuth()
+}, [])
+useEffect( () => {
+  async function loadProducts(){
+    const Bearer = 'Bearer '.concat(token)
+    const {data} = await api.get('products', {headers : { Authorization : Bearer}})
+    setProducts(data)
+  }
+  loadProducts()
+}, [token])
+
+
+  async function handleNewProduct(e){
+    e.preventDefault();
+
+    if ( name !== "" && stock !== '' && price !== ""){
+      const res = await api.post('/products', {name, price, stock, token})
+      if (res){
+        setAlert("Cadastrado com Sucesso")
+        console.log(res)
+      }
+    }
+    else{
+      setAlert("Preencha todos os campos")
+    }
+  }
+
+  function handleSelectItem({id}) {
+    if (selectedItem.includes(id)){
+        const filteredItems = selectedItem.filter( item => item !== id) ;
+        setSelectedItem(filteredItems)
+    }else{
+      setSelectedItem([...selectedItem, id])
+    }
+  }
+
+  async function handleNewOrder(){
+    if (selectedItem.length > 0){
+      let status = "processing"
+      await api.post('/orders', {status, token}).then( ({data}) => {
+       const {id} = data
+        selectedItem.forEach( async (item) => {
+          console.log({id, item})
+          await api.post('/items-order', {order_id : id, product_id: item, token}).then( data => {
+            if(data){
+              setSelectedItem('')
+            }
+          })
+        })
+      })
+    }
+  }
   return (
     <div id="products-page">
         <Menu/>
         <div className="content">
             <h1>Produtos</h1>
             <h3>Cadastre um novo produto</h3>
-            <form>
-                <input type="text" placeholder="Nome do produto" className="input-large"/>
-                <input type="text" placeholder="Valor Unitário" className="input"/>
-                <input type="text" placeholder="Quantidade" className="input"/>
-                <button>Cadastrar</button>
+            <form onSubmit={handleNewProduct}>
+                <input type="text" placeholder="Nome do produto" className="input-large" value={name} onChange={ e => setName(e.target.value)}  />
+                <input type="text" placeholder="Valor Unitário" className="input" value={price} onChange={ e => setPrice(e.target.value)}/>
+                <input type="number" placeholder="Quantidade" className="input" value={stock} onChange={ e => setStock(e.target.value)}/>
+                <button type='submit'>Cadastrar</button>
             </form>
+            {
+              alert && (
+                <p id="alert">{alert}</p>
+              )
+            }
+            <div id="products-list">
+              <ul>
+                <li className="product-item strong">
+                  <p>Código</p>
+                  <p>Nome</p>
+                  <p>Valor</p>
+                  <p>Estoque</p>
+                </li>
+                {products.map( item => 
+                    <li key={item.id} onClick={() => handleSelectItem(item)} className={ selectedItem.includes(item.id) ? "product-item selected" : "product-item"} >
+                      <p>{item.id}</p>
+                      <p>{item.name}</p>
+                      <p>R$ {item.price}</p>
+                      <p>{item.stock}</p>
+                    </li>
+                )}  
+              </ul>
+            </div>
+            <div id="new-order">
+              <button className="large" onClick={handleNewOrder}>Novo Pedido</button>
+              <p>*Clique nos produtos que você deseja inserir no pedido</p>
+            </div>
         </div>
     </div>
   )
